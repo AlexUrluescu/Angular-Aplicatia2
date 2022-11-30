cac6fe1e778ced539aa92c1a73777df2eee2fd0b
Alexandre <alexurluescu23@gmail.com>
2022-11-30 19:21:18 +0200

    M-am jucat putin cu componentele

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersoaneComponent } from './persoane/persoane.component';
+import { BunVenitComponent } from './bun-venit/bun-venit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersoaneComponent,
+    BunVenitComponent
   ],
   imports: [
     BrowserModule,
